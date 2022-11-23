8b117543c7c511ea2f6d21463ec3a3599c21d5b8
souravrn01 <112268288+souravrn01@users.noreply.github.com>
2022-11-23 11:31:45 +0530

    made navbar and dashboard component in admin

--- frontend/src/app/admin/admin.module.ts
+++ frontend/src/app/admin/admin.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavbarComponent,
+    DashboardComponent
+  ],
   imports: [
     CommonModule,
     AdminRoutingModule
