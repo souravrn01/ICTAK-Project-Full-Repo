28656baf7eb0ae012db083add26e458727c286fd
sherin17 <112325140+sherin17@users.noreply.github.com>
2022-11-23 09:55:48 +0530

    material module added sherin

---
+++ frontend/src/app/material/material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+
+
+const MaterialComponents = [
+  
+];
+
+@NgModule({
+  declarations: [],
+  imports: [],
+  exports:[]
+  
+})
+export class MaterialModule { }
