ccff37ced7fa7fd703782c9c8f39172445ad8ec2
souravrn01 <112268288+souravrn01@users.noreply.github.com>
2022-11-24 12:07:57 +0530

    made all required schema

--- backend/middlewares/mongodb.js
+++ backend/middlewares/mongodb.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://Sherin17:Reenijiji1@cluster0.quvjjgb.mongodb.net/WebJobPortal?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://Sherin17:Reenijiji1@cluster0.quvjjgb.mongodb.net/WebJobPortal?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 . then(()=>{
     console.log('mongoDB connected successfully!!!')
 }).catch((error)=>{
