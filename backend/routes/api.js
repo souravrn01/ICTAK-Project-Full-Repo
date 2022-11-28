a365acc9d286379d45657383d25691c2b6322534
sherin17 <112325140+sherin17@users.noreply.github.com>
2022-11-28 18:09:40 +0530

    get job data

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -5,10 +5,12 @@ const router = express.Router()
 
 // below declare all required api
 const verifiedJobs = require('./jobApi')
-const Alumini=require('./alumniApi')
+// const Alumini=require('./alumniApi')
+const alumniUV = require('./alumniApiUV')
 
 
 //use api
 router.use('/verifiedjobs',verifiedJobs)
-router.use('/Alumini',Alumini)
+// router.use('/Alumini',Alumini)
+router.use('/alumniuv',alumniUV)
 module.exports = router;
