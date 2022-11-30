bd93397d2092b3fcbbc54ff4d64cca19ed70da33
kuttyshanu <106152043+kuttyshanu@users.noreply.github.com>
2022-11-30 12:15:22 -0800

    1 commit

--- frontend/src/app/admin/admin-dash.component.ts
+++ frontend/src/app/admin/admin-dash.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { OwlOptions } from 'ngx-owl-carousel-o';
 @Component({
   selector: 'app-admin-dash',
   templateUrl: './admin-dash.component.html',
@@ -11,5 +11,28 @@ export class AdminDashComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 4
+      }
+    },
+    nav: true
+  }
 }
