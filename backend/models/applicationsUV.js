b1bc442895b3039f862cf24e6efc1548ed957be9
sherin17 <112325140+sherin17@users.noreply.github.com>
2022-12-01 11:15:07 +0530

    alumno job application

--- backend/models/applicationsUV.js
+++ backend/models/applicationsUV.js
@@ -1,1 +1,17 @@
 // UNVERIFIED APPLICATIONS SCHEMA
+const mongoose=require('mongoose')
+const schema=mongoose.Schema
+
+const applicationSchema= new schema({  
+
+    name:String,
+    email:String,
+    phone:Number,
+    resume_file_upload:String,
+    profile_link:String,
+    approval_status:{
+        type: String,
+        default: "not approved"
+    },
+
+}) 
