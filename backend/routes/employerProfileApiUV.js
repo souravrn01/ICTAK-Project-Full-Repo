3d41b6cf0981cb3680c4902708f64771b9d19966
souravrn01 <112268288+souravrn01@users.noreply.github.com>
2022-12-02 11:01:03 +0530

    employer admin backend

--- backend/routes/employerProfileApiUV.js
+++ backend/routes/employerProfileApiUV.js
@@ -29,6 +29,17 @@ router.get('/employelist',async(req,res)=>{
        console.log(error) 
     }
 })
+
+router.get('/employers',async(req,res)=>{       // getdata for admin to collect unverified employer
+    try {
+        let list=await EmployeData.find({approval_status: "not approved"})
+        res.send(list)
+    } catch (error) {
+       console.log(error) 
+    }
+})
+
+
 router.get('/employesignup/:id',async(req,res)=>{
     try {
         let id=req.params.id
@@ -39,6 +50,38 @@ router.get('/employesignup/:id',async(req,res)=>{
     }
 })
 
+router.delete('/deleteemployer/:id', async(req,res)=>{    // admin deleting employer
+    try {
+        let id = req.params.id
+        let data = await EmployeData.findByIdAndDelete(id)
+        res.send(data)
 
+    } catch (error) {
+        console.log(error)
+    }   
+})
+
+
+router.put('/verifyemp', async(req,res)=>{    // for admin to get employer to verify
+    try {
+
+        console.log(req.body)
+        let id = req.body._id
+        let update = { 
+            name: req.body.name,
+            email: req.body.email,
+            phone:  req.body.phone,
+            company: req.body.company,
+            approval_status:  req.body.approval_status,
+            password:  req.body.password,
+            designation: req.body.designation
+        }
+        let updates = {$set: update}
+        let verifiedEmp = await EmployeData.findByIdAndUpdate({"_id": id}, updates,{new:true})
+        res.send(verifiedEmp)
+
+} catch (error) {
+    console.log('update error:',error);
+}})
 
 module.exports=router;
--- frontend/src/app/admin/admin-routing.module.ts
+++ frontend/src/app/admin/admin-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminDashComponent } from './admin-dash.component';
 import { AlumniUVComponent } from './components/alumni-uv/alumni-uv.component';
 import { JobPostFormComponent } from './components/jobPostForm/job-post-form.component';
+import { VerifyEmpComponent } from './components/verify-emp/verify-emp.component';
 
 
 const routes: Routes = [
   {path:'adminDash', component:AdminDashComponent},
   {path:'jobpost', component:JobPostFormComponent},
-  {path:'alumniverify', component:AlumniUVComponent},
+  {path:'verifyalumni', component:AlumniUVComponent},
+  {path:'verifyemp', component:VerifyEmpComponent},
   
 ];
 
