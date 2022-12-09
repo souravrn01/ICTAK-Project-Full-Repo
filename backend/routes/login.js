847bf96fa66f5549d31714ba383649b6289f1279
sherin17 <112325140+sherin17@users.noreply.github.com>
2022-12-09 13:57:40 +0530

    admin token creation finished

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -2,22 +2,27 @@ const express = require('express')
 const router = express.Router()
 const jwt = require('jsonwebtoken')
 
-// email='admin@gmail.com';
-// password='Admin@123';
+email='admin@gmail.com';
+password='Admin@123';
 
 router.post('/loginadmin',async(req,res)=>{
     try{
         console.log('From frontend auth token',req.headers.authorization)
-        let email=req.body.email;
-        let password=req.body.password;
-        let payload= {
-            'email':email,
-            'password':password,
-            'date':Date.now()
+        
+        if(email===req.body.email && password===req.body.password){
+            let payload= {
+                'email':email,
+                'password':password,
+                'date':Date.now()
+            }
+            let token = await jwt.sign(payload,'secretKey')
+            res.send({token})
         }
-        let token = await jwt.sign(payload,'secretKey')
-        res.send({token})
-    }
+        else{
+            return res.json({ message: " Invalid Username and password !!" });
+        }
+       
+    }  
         catch(error){
             console.log(error)
         }
