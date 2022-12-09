654290c1291e02f01655f9a5458ddb46dad4b0f1
shijinajs <112337210+shijinajs@users.noreply.github.com>
2022-12-09 05:24:23 -0800

    Update jwtVerify.js

--- backend/middlewares/jwtVerify.js
+++ backend/middlewares/jwtVerify.js
@@ -1,20 +1,25 @@
-const jwt=require('jsonwebtoken')
+const {JsonWebTokenError} = require("jsonwebtoken")
+const jwt = require('jsonwebtoken')
 
-const verifytoken=(req,res,next)=>{
- if(!req.headers.authorization){
-    return res.status(401).send('Unauthorized request')
- }
- const token=req.headers.authorization
- if(token===null){
-    return res.status(401).send('Unauthorized request')
- }
- jwt.verify(token,'secretkey',(err,payLoad)=>{
-    if(err){
-        return res.status(401).send('Unauthorized request')
+//middleware
+function verifytoken (req, res, next) {
+    console.log('headers=', req.headers.authorization);
+    if (!req.headers.authorization) {
+        return res.status(401).send('Unautherized request');
     }
-    req.payload=payLoad
-    next();
- })
-} 
+    let token = req.headers.authorization.split(' ')[1];
+    if (token == 'null') {
+        return res.status(401).send('Unautherized request');
+    }
+    let payload = jwt. verify(token , 'secretkey');
+    console.log("payload=", payload);
+    if (!payload) {
+        return res.status(401).send('Unautherized request');
+    }
+    console.log("payload.subject=", payload.subject);
 
-module.exports=verifytoken
+    req.userId = payload.subject;
+    next();
+
+}
+module.exports = verifytoken
